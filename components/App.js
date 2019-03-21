fa2411f2a6bf99f60af428a765a509794d55bf97
สุรีพร เรืองเดช <s6002041520174@Macmini-213-19.local>
2019-03-21 16:38:39 +0700

    Update App.js

--- components/App.js
+++ components/App.js
@@ -7,10 +7,15 @@ class App extends React.Component{
         return(
                 //component about
                 <View>
+<<<<<<< HEAD
                     <Text style={{fontSize: 40 }}>
                         Suriporn Rueangdet  
                         
                          
+=======
+                    <Text style={{fontSize: 50 }}>
+                        Suriporn Rueangdet 
+>>>>>>> 0dfd1f36f0031cd2cb782e843f27d8946c6cbd5c
                     </Text>
                 </View>
         );
